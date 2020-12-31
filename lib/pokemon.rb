029ad5c6792a0e252fa5a27c4da2e39f8db5422f
dlepperts <danielle.leppertsimenauer@gmail.com>
2020-12-31 15:22:26 -0600

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,25 +11,23 @@ class Pokemon
     end
 
     # Saves an instance of a pokemon with the correct id.
-    def self.save(id, name, type)
+    def self.save(name, type, db)
         sql = <<-SQL
         INSERT INTO pokemon(name, type)
         VALUES (?,?)
         SQL
 
-        DB[:conn].execute(sql, name, type)
+        db.execute(sql, name, type)
 
-        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
     end
 
-    # Finds a pokemon from the database by their id number and returns a new Pokemon object 
-    def self.find(id)
-        sql = <<-SQL
-        SELECT * FROM pokemon
-        WHERE id = ?
-        SQL
 
-        DB[:conn].execute(sql, id)
+    # Finds a pokemon from the database by their id number and returns a new Pokemon object 
+    def self.find(id, db)
+        sql = "SELECT * FROM pokemon WHERE id = ?"
+        result = db.execute(sql, id)[0]
+        self.new(id: result[0], name: result[1], type: result[2], db: @db)
     end
 
 end
